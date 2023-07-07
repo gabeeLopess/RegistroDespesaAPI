83446fde461408eb37619d16a0b51fd333bb8e8a
Gabriel Lopes <ga04789135373@gmail.com>
2023-07-07 11:57:06 -0300

    comi

---
+++ API/RegistroDespesaApi/RegistroDespesaApi/Base/RegistroDespesaDb.cs
@@ -0,0 +1,14 @@
+﻿using RegistroDespesaApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroDespesaApi.Base
+{
+    public class RegistroDespesaDb : DbContext
+    {
+        public RegistroDespesaDb(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<DespesaModel> Despesas { get; set; }
+    }
+}
---
+++ API/RegistroDespesaApi/RegistroDespesaApi/Models/DespesaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistroDespesaApi.Models
+{
+    public class DespesaModel
+    {
+        public int codDespesa { get; set; }
+        public string NomeDespesa { get; set; }
+        public string DescricaoDespesa { get; set; }
+        public float Valor { get; set; }
+        public DateTime Data { get; set; }
+        public float ValorTotal { get; set; }
+    }
+}
